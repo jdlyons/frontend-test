b7c14f80ec2bf1753fc44c5c6f745b45b40693e5
Joe Lyons <joseph.lyons@sitekit.net>
2021-07-12 19:15:29 +0100

    #5 - Add mechanism to create new tasks with title and importance level(values 0, 1, or 2) that POSTs to / api/task and updates the UI with the task list. Both title and importance are required and should be validated on the client-side

--- README.md
+++ README.md
@@ -38,9 +38,9 @@ This repo includes webpack (https://webpack.js.org/concepts) and includes suppor
 | 1 | ~~Install dependencies, run the app and api, and have a play around~~ |
 | 2 | ~~GET the list of Tasks from /api/task and render tasks to the page~~ |
 | 3 | ~~Order each task using the importance property (0 = high, 1 = medium, 2 = low) and split into two lists based on the isDone property (0 - no done, 1 - done). Your prototype must be similar to the supplied [wireframe sketch](wireframe/wireframe.svg).~~
-| 4 | Your prototype should be accessible to WCAG 2.1 AA compliance. Ensure that your UI is accessible and ensure further features added in tasks are accessible.
-| 5 | Add mechanism to create new tasks with title and importance level (values 0, 1, or 2) that POSTs to /api/task and updates the UI with the task list. Both title and importance are required and should be validated on the client-side |
-| 6 |  There is an artificial 500ms latency on all GET and POST API requests. Improve the user experience by adding a loading state to the UI |
+| 4 | ~~Your prototype should be accessible to WCAG 2.1 AA compliance. Ensure that your UI is accessible and ensure further features added in tasks are accessible.~~
+| 5 | ~~Add mechanism to create new tasks with title and importance level (values 0, 1, or 2) that POSTs to /api/task and updates the UI with the task list. Both title and importance are required and should be validated on the client-side |~~
+| 6 | There is an artificial 500ms latency on all GET and POST API requests. Improve the user experience by adding a loading state to the UI |
 | 7 | Implement a mechanism to mark a Task as done on the UI and PATCH /api/task/<task.id> so a change to the isDone property of a task persists on page reload. The complete task should go into a separate list as suggested in the [wireframe sketch](wireframe/wireframe.svg) |
 | 8 | Add a control to allow a user to delete a Task, making a DELETE request to /api/task/<task.id> and updating the UI |
 | 9 | Implement a mechanism to edit a task that allows a user to change the label and importance by PATCHing /api/task/<task.id> |
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,12 @@
 import '../css/index.scss';
 
 document.addEventListener("DOMContentLoaded", () => {
-    contentFetcher.getContent("http://localhost:4000/api/tasks");
+    document.body.innerHTML = "";
+
+    contentFetcher.getContent("http://localhost:4000/api/tasks")
+        .then(() => {
+            newTask.getForm("http://localhost:4000/api/tasks");
+        });
 });
 
 let contentFetcher = {
@@ -12,36 +17,90 @@ let contentFetcher = {
             method: "get"
         })
 
-        .then((response) => {
-            if (!response.ok) {
-                contentFetcher.error(errorMessage);
-                throw Error(response.statusText);
-            }
+            .then((response) => {
+                if (!response.ok) {
+                    contentFetcher.error(errorMessage);
+                    throw Error(response.statusText);
+                }
 
-            return response.text();
-        })
+                return response.text();
+            })
 
-        .then((data) => {
-            document.body.innerHTML = "";
-            let taskList = document.createElement("ul");
+            .then((data) => {
+                let taskList = document.createElement("ul");
+                taskList.id = "tasks"
 
-            JSON.parse(data)
-                .sort((a, b) => {
-                    if (a.importance < b.importance) {
-                        return -1;
-                    }
-                })
-                .map((task) => {
-                    let listItem = document.createElement("li");
-                    listItem.innerHTML = "<label><input type='checkbox'" + (task.isDone === "1" ? "checked='checked'" : "") + " /><span>" + task.title + "</span></label>";
-                    taskList.appendChild(listItem);
-                });
+                JSON.parse(data)
+                    .sort((a, b) => {
+                        if (a.importance < b.importance) {
+                            return -1;
+                        }
+                    })
+                    .map((task) => {
+                        let listItem = document.createElement("li");
+                        listItem.innerHTML = "<label><input type='checkbox'" + (task.isDone === "1" ? "checked='checked'" : "") + " /><span>" + task.title + "</span></label>";
+                        taskList.appendChild(listItem);
+                    });
 
-            document.body.appendChild(taskList);
-        });
+                document.body.appendChild(taskList);
+            });
     },
 
     error: (message) => {
+        document.body.innerHTML = "<p>There was an error retrieving the tasks - please try reloading the page.</p><p>If the error persists, please try again later or contact the site owner if possible.</p>";
+        console.warn(message);
+    }
+};
 
+let newTask = {
+    getForm: (action) => {
+        let newTask = document.createElement("li");
+        newTask.innerHTML = "<form method='post' action='" + action + "' id='newTask'>+<input type='text' name='title' placeholder='Title' id='title' required='required' title='New task title' value='' /><input type='number' name='importance' placeholder='Importance' id='importance' required='required' step='1' min='0' max='2' value='' /><input type='submit' value='Create' title='New task importance' /></form>";
+
+        let taskList = document.getElementById("tasks");
+        taskList.append(newTask);
+
+        document.addEventListener("submit", (e) => {
+            if (e.target.id === "newTask") {
+
+                let form = e.target,
+                    fields = e.target.querySelectorAll('input'),
+                    data = new FormData(form),
+                    formObj = Object.fromEntries(data);
+
+                [].forEach.call(fields, (field) => {
+                    field.setAttribute("disabled", "disabled");
+                });
+
+                fetch(action, {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(formObj),
+                })
+
+                .then((response) => {
+                    if (response.ok) {
+                        return response.json();
+                    }
+                    return Promise.reject(response);
+                })
+
+                .then((data) => {
+                    window.location.reload();
+                })
+
+                .catch((error) => {
+                    [].forEach.call(fields, (field) => {
+                        field.removeAttribute("disabled", "disabled");
+                    });
+                    window.location.reload();
+                    console.warn(error);
+                });
+            }
+            e.preventDefault();
+        });
     }
 }
